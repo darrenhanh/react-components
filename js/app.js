37dc8b832b957da4455d9b3f101da0b7c625f210
hanh@6678909 <hhj@joinforwin.com>
2017-02-27 17:50:46 +0800

    s

---
+++ js/app.js
@@ -0,0 +1,34 @@
+import React, { Component, PropTypes, cloneElement } from 'react';
+import ReactDOM from 'react-dom';
+import styles from '../css/style.less';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {activeIndex: 0};
+  }
+
+  handleChange(e) {
+    this.setState({activeIndex: parseInt(e.target.value, 10)});
+  }
+
+  render() {
+    return (
+      <div>
+        <div className={styles.operator}>
+          <span>切换 Tab：</span>
+          <select value={this.state.activeIndex} onChange={this.handleChange}>
+            <option value="0">Tab 1</option>
+            <option value="1">Tab 2</option>
+            <option value="2">Tab 3</option>
+          </select>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
